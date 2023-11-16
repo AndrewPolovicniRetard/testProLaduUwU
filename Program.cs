7dfbfabcdb25a00316a7f342dbe4df32376e0a46
AndrewPolovicniRetard <1155@student.itgymnazium.cz>
2023-11-16 10:25:36 +0100

    Initial commit for Ladislav

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitTestProLadu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cau Lado, jak se mas??");
+        }
+    }
+}
